a5fb576ca7a9eae69463e1b766c8b954d5a1472d
sotiriosc <chortogiannoss@gmail.com>
2023-05-05 09:07:15 -0400

    added logic for install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,50 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior (browser prompting the user)
+  event.preventDefault();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Store the event so it can be triggered later
+  window.deferredPrompt = event;
+});
+
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  // Check if there's a deferred prompt stored
+  if (window.deferredPrompt) {
+    const promptEvent = window.deferredPrompt;
+
+    // Show the prompt to the user
+    promptEvent.prompt();
+
+    // Wait for the user's response
+    const userChoice = await promptEvent.userChoice;
+
+    // Check if the user accepted the prompt
+    if (userChoice.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+
+    // Clear the deferred prompt
+    window.deferredPrompt = null;
+  }
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA installed successfully.');
+  // Show an alert with the outcome of the installation
+  alert('PWA installed successfully.');
+});
+
+// Add an event listener for the error during installation
+window.addEventListener('appinstallerror', (event) => {
+  console.error('PWA installation failed:', event);
+  // Show an alert with an error message
+  alert('Install not successful, Browser may not support APP');
+});
