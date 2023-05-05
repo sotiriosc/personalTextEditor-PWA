0b631cccac8f11e97927a4ab9e0941e2659d35c9
sotiriosc <chortogiannoss@gmail.com>
2023-05-05 08:52:27 -0400

    corrected src-sw script

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
